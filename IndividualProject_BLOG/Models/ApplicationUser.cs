32adbdd932edd39f9b55ee5a40b9b4f71e7a1442
OlegDimitrov <olegsdimitrov@gmail.com>
2016-08-24 21:07:03 +0300

    Model Class changed

--- IndividualProject_BLOG/Models/ApplicationUser.cs
+++ IndividualProject_BLOG/Models/ApplicationUser.cs
@@ -2,13 +2,16 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.EntityFramework;    
+using System.ComponentModel.DataAnnotations;
 
 namespace IndividualProject_BLOG.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(100)]
+        public string FullName { get; set; }  
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -18,16 +21,5 @@ namespace IndividualProject_BLOG.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+    
 }
