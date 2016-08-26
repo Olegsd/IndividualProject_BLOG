a0db570745936792031af91e4bad1d0e3285cafa
OlegDimitrov <olegsdimitrov@gmail.com>
2016-08-26 21:06:15 +0300

    Automatic Migration

---
+++ IndividualProject_BLOG/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace IndividualProject_BLOG.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<IndividualProject_BLOG.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "IndividualProject_BLOG.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(IndividualProject_BLOG.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- IndividualProject_BLOG/Models/Post.cs
+++ IndividualProject_BLOG/Models/Post.cs
@@ -17,7 +17,7 @@ namespace IndividualProject_BLOG.Models
         public int Id { get; set; }
 
         [Required]
-        [StringLength(200)]
+        [StringLength(250)]
         public string Title { get; set; }
 
         [Required]
