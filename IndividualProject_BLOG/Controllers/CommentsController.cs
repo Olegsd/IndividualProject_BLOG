b7581002a9038e8deb52f9f9182696d8da33b925
OlegDimitrov <olegsdimitrov@gmail.com>
2016-08-28 00:23:39 +0300

    insert and update automatic CRUD for comments

---
+++ IndividualProject_BLOG/Controllers/CommentsController.cs
@@ -0,0 +1,141 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Authentication.ExtendedProtection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using IndividualProject_BLOG.Models;
+using Microsoft.AspNet.Identity;
+using WebGrease.Css.Extensions;
+
+namespace IndividualProject_BLOG.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Comments
+        public ActionResult Index()
+        {
+            var comments = db.Comments.Include(c => c.Author);
+            return View(comments.ToList());
+        }
+
+        // GET: Comments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            ViewBag.Author_Id = new SelectList(db.Users, "Id", "FullName");
+            return View();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Text")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.Author = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Author_Id = new SelectList(db.Users, "Id", "FullName", comment.Author_Id);
+            return View(comment);
+        }
+
+        // GET: Comments/Edit/5
+        [Authorize(Roles = "Administrators")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Author_Id = new SelectList(db.Users, "Id", "FullName", comment.Author_Id);
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "Administrators")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Text,Date,Author_Id")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Author_Id = new SelectList(db.Users, "Id", "FullName", comment.Author_Id);
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        [Authorize(Roles = "Administrators")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Administrators")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
